2cd1605cd2a895121529b7cf4d5960d6b1c5218d
Nohx14 <george11mutua@gmai.com>
2023-03-26 13:26:15 +0000

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -16,18 +16,18 @@ void print_most_numbers(void);
 
 void print_most_numbers(void)
 {
- int number;
+	int number;
 
- for (number = 0; number <= 9; number++)
- {
- if (number == 2 || number == 4)
-  {
- continue;
-  }
- else
-  {
-   _putchar(number + '0');
-  }
- }
- _putchar('\n');
+	for (number = 0; number <= 9; number++)
+	{
+		if (number == 2 || number == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(number + '0');
+		}
+	}
+	_putchar('\n');
 }
